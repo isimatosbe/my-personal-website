a7a7f6d020cd7ebd233d3ef63126d618fd7c4830
Isidro Matos Bellido <isimatosbe@gmail.com>
2025-01-31 17:59:45 +0100

    Dropped section added and projects section updated

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,8 +7,9 @@ export default function Projects({ lang }) {
     const sortedProjects = 
         projects["data"].sort((a, b) => new Date(a.startingDate) <= new Date(b.startingDate) ? 1 : -1)
 
-    const underDevelopment = sortedProjects.filter( project => project.live)
-    const finishedProjects = sortedProjects.filter( project => !project.live)
+    const underDevelopment = sortedProjects.filter( project => project.status === "live")
+    const finishedProjects = sortedProjects.filter( project => project.status === "finished")
+    const droppedProjects = sortedProjects.filter( project => project.status === "dropped")
 
     return (
         <>
@@ -30,6 +31,18 @@ export default function Projects({ lang }) {
             <h3 className='semi-bold'>{projects["sections"][lang]["finished"]}</h3>
             <ul>
                 {finishedProjects.map( project =>
+                    <Project
+                        key={project[lang]["title"]}
+                        title={project[lang]["title"]} 
+                        url={project.url}
+                        tags={project.tags} 
+                        description={project[lang]["description"]} />
+            )}
+            </ul>
+
+            <h3 className='semi-bold'>{projects["sections"][lang]["dropped"]}</h3>
+            <ul>
+                {droppedProjects.map( project =>
                     <Project
                         key={project[lang]["title"]}
                         title={project[lang]["title"]} 
